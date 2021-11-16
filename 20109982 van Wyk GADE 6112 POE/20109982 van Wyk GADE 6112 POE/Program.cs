80a53eaed241825b91bb6c69989015a94b129696
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2021-11-16 21:50:44 +0200

    Changes

---
+++ 20109982 van Wyk GADE 6112 POE/20109982 van Wyk GADE 6112 POE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20109982_van_Wyk_GADE_6112_POE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
